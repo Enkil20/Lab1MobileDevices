2b0d1729d1ac5919c963e62c5e1e8694b83e123e
Arturo Salinas Ortiz <salor_arturo@hotmail.com>
2018-01-19 14:23:32 -0600

    Second commit: Changed the "hello world"

--- app/src/main/java/salor/firstlab/LAB1.java
+++ app/src/main/java/salor/firstlab/LAB1.java
@@ -1,10 +1,12 @@
 package salor.firstlab;
 
+//import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LAB1 extends AppCompatActivity {
-
+    //This is my very first app in Android Studio!!!
+    //By Enkil (Arturo Salinas Ortiz A01335189)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
